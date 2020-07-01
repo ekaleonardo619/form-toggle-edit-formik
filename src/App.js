7d4be389774e34df7661a7d2ee4a39c974335c75
Eka Leonardo <ekaleonardo@Ekas-MacBook-Air.local>
2020-07-01 13:48:14 +0700

    Initial Commit Formik

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import FormikForm from './FormikForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+  const [fields, updateFields] = useState(
+    {
+      name: 'Admin',
+      email: 'admin@example.com',
+      mobile_no: '012345678'
+    }
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <FormikForm fields={fields} updateFields={updateFields}/>
     </div>
   );
 }
